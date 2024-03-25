eefb888d17f8fcf0ace251891da06f1fb95d6a84
« ⟪ DummerGoki ⟫ » <125484879+DummerGoki@users.noreply.github.com>
2024-03-25 16:00:22 +0100

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -5,12 +5,22 @@
 #------------------------------------------#
 steam="C:/Program Files (x86)/Steam/"
 #------------------------------------------#
-# start=0 (it will NOT run gmod)           #
-# start=1 (it will start automaticly gmod) #
+# start=0 (it will do nothing)             #
+# start=1 (it will start automaticly gmod) # 
 #------------------------------------------#
 start=1
+#------------------------------------------#
+# addon=0 (it will do nothing)             #
+# addon=1 (it will remove all addon files) # <trusted> 
+#------------------------------------------#
+addon=0
+#------------------------------------------#
+# materials=0 (it will do nothing)         # 
+# materials=1 (it will delete materials)   # <trusted>
+#------------------------------------------#
+material=0
 #------------------------------------------# 
-# config=0 (it will not remove configs)    # <this is less safe>
+# config=0 (it will do nothing)            # 
 # config=1 (it will remove config files)   # <trusted>    
 #------------------------------------------#
 config=0
